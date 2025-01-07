9c3fedbc086ce6413b66272065a15c7c2c807555
samuelbadman <samuelbadman@outlook.com>
2025-01-07 16:48:57 +0000

    Set game player camera manager class in game player controller

---
+++ Source/Project1/Public/PlayerCameraManagers/GamePlayerCameraManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PlayerCameraManagers/Project1PlayerCameraManagerBase.h"
+#include "GamePlayerCameraManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT1_API AGamePlayerCameraManager : public AProject1PlayerCameraManagerBase
+{
+	GENERATED_BODY()
+	
+};
