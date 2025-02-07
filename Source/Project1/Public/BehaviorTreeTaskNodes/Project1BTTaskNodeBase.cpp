8ed33fecd96241b65dafbbb3a24caadd5ea6891d
samuelbadman <samuelbadman@outlook.com>
2025-02-07 16:27:39 +0000

    Removed behavior tree example

--- Source/Project1/Public/BehaviorTreeTaskNodes/Project1BTTaskNodeBase.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Project1BTTaskNodeBase.h"
-
-UProject1BTTaskNodeBase::UProject1BTTaskNodeBase()
-{
-	bNotifyTick = false;
-}
