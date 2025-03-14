a68d1cf779f63db5dfa8a693a76a82fe436e93e0
samuelbadman <samuelbadman@outlook.com>
2025-03-14 13:46:35 +0000

    Added prototype male character skeletal mesh assets

---
+++ Source/Project1/Public/AnimInstances/PrototypeMaleCharacterAnimInst.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AnimInstances/Project1AnimInstanceBase.h"
+#include "PrototypeMaleCharacterAnimInst.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT1_API UPrototypeMaleCharacterAnimInst : public UProject1AnimInstanceBase
+{
+	GENERATED_BODY()
+	
+};
