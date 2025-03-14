b1cafe77616ab726eaae94d585714320790110ce
samuelbadman <samuelbadman@outlook.com>
2025-03-14 10:44:18 +0000

    Added Manny simple mesh and animation instance to test AI character and updated Manny simple walk cycle starting frame to passing pose for better blend in behaviour

--- Source/Project1/Public/Objects/AI/Goals/Goal_Test_Idle.cpp
+++ Source/Project1/Public/Objects/AI/Goals/Goal_Test_Idle.cpp
@@ -38,7 +38,7 @@ void UGoal_Test_Idle::AddMoveToRandomReachablePointSubGoal(TObjectPtr<AProject1A
 	const TObjectPtr<UGoal_Common_MoveToLocation> MoveToLocationGoal{ AI->CreateGoal<UGoal_Common_MoveToLocation>(UGoal_Common_MoveToLocation::StaticClass()) };
 	FNavLocation TargetMoveLocation;
 	FNavigationSystem::GetCurrent<UNavigationSystemV1>(GetWorld())->GetRandomReachablePointInRadius(FVector::ZeroVector, 2048.0f, TargetMoveLocation);
-	MoveToLocationGoal->Initialize(TargetMoveLocation.Location, ECharacterGroundMovementState::Run, 50.0f);
+	MoveToLocationGoal->Initialize(TargetMoveLocation.Location, ECharacterGroundMovementState::Walk, 50.0f);
 	AI->AddSubGoal(MoveToLocationGoal);
 	bJustMoved = true;
 }
