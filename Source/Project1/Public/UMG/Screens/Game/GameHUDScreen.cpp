a1c5453df72599571cc2b7cf242876ad3ebcb00f
samuelbadman <samuelbadman@outlook.com>
2025-04-10 16:30:00 +0100

    Added feature to fade in and out the entire game hud screen optionally after a delay.

--- Source/Project1/Public/UMG/Screens/Game/GameHUDScreen.cpp
+++ Source/Project1/Public/UMG/Screens/Game/GameHUDScreen.cpp
@@ -3,9 +3,78 @@
 
 #include "GameHUDScreen.h"
 
-void UGameHUDScreen::NativeOnInitialized()
+UGameHUDScreen::UGameHUDScreen()
+	:
+	TargetScreenRenderOpacity(1.0f),
+	CurrentScreenFadeInterpRate(0.0f),
+	bShouldUpdateRenderOpacity(false),
+	ScreenFadeDelayTimerHandle({})
 {
-	Super::NativeOnInitialized();
+}
 
+void UGameHUDScreen::FadeScreen(EGameHUDScreenFadeDirection Direction, float Rate)
+{
+	FTimerManager& TimerManager{ GetWorld()->GetTimerManager()};
 
+	if (TimerManager.IsTimerActive(ScreenFadeDelayTimerHandle))
+	{
+		TimerManager.ClearTimer(ScreenFadeDelayTimerHandle);
+	}
+
+	switch (Direction)
+	{
+	case EGameHUDScreenFadeDirection::Out: BeginScreenFade(ScreenFadeOutTargetRenderOpacity, Rate); break;
+	case EGameHUDScreenFadeDirection::In: BeginScreenFade(ScreenFadeInTargetRenderOpacity, Rate); break;
+	}
+}
+
+void UGameHUDScreen::FadeScreenAfterDelay(EGameHUDScreenFadeDirection Direction, float Rate, float DelayDuration)
+{
+	FTimerDelegate TimerDel{};
+	TimerDel.BindUObject(this, &UGameHUDScreen::FadeScreen, Direction, Rate);
+	GetWorld()->GetTimerManager().SetTimer(ScreenFadeDelayTimerHandle, TimerDel, DelayDuration, false);
+}
+
+void UGameHUDScreen::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	Super::NativeTick(MyGeometry, InDeltaTime);
+
+	if (bShouldUpdateRenderOpacity)
+	{
+		float UpdatedRenderOpacity{ FMath::FInterpConstantTo(GetRenderOpacity(), TargetScreenRenderOpacity, InDeltaTime, CurrentScreenFadeInterpRate) };
+
+		if (FMath::IsNearlyEqual(UpdatedRenderOpacity, TargetScreenRenderOpacity))
+		{
+			UpdatedRenderOpacity = TargetScreenRenderOpacity;
+			bShouldUpdateRenderOpacity = false;
+
+			if (TargetScreenRenderOpacity == ScreenFadeOutTargetRenderOpacity)
+			{
+				OnScreenFadeOutComplete();
+			}
+			else if (TargetScreenRenderOpacity == ScreenFadeInTargetRenderOpacity)
+			{
+				OnScreenFadeInComplete();
+			}
+		}
+
+		SetRenderOpacity(UpdatedRenderOpacity);
+	}
+}
+
+void UGameHUDScreen::BeginScreenFade(float TargetRenderOpacity, float FadeRate)
+{
+	TargetScreenRenderOpacity = TargetRenderOpacity;
+	CurrentScreenFadeInterpRate = FadeRate;
+	bShouldUpdateRenderOpacity = true;
+}
+
+void UGameHUDScreen::OnScreenFadeOutComplete()
+{
+	ScreenFadeOutComplete.Broadcast();
+}
+
+void UGameHUDScreen::OnScreenFadeInComplete()
+{
+	ScreenFadeInComplete.Broadcast();
 }
--- Source/Project1/Public/UMG/Screens/Game/GameHUDScreen.h
+++ Source/Project1/Public/UMG/Screens/Game/GameHUDScreen.h
@@ -8,8 +8,18 @@
 
 class UCompassBarUserWidget;
 
+UENUM()
+enum class EGameHUDScreenFadeDirection : uint8
+{
+	In,
+	Out
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnScreenFadeOutCompleteSignature);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnScreenFadeInCompleteSignature);
+
 /**
- * 
+ *
  */
 UCLASS()
 class UGameHUDScreen : public UScreenUserWidgetBase
@@ -17,9 +27,39 @@ class UGameHUDScreen : public UScreenUserWidgetBase
 	GENERATED_BODY()
 
 public:
+	UPROPERTY(BlueprintAssignable)
+	FOnScreenFadeOutCompleteSignature ScreenFadeOutComplete;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnScreenFadeInCompleteSignature ScreenFadeInComplete;
+
+private:
+	static constexpr float ScreenFadeOutTargetRenderOpacity{ 0.0f };
+	static constexpr float ScreenFadeInTargetRenderOpacity{ 1.0f };
+
+	float TargetScreenRenderOpacity;
+	float CurrentScreenFadeInterpRate;
+	bool bShouldUpdateRenderOpacity;
+	FTimerHandle ScreenFadeDelayTimerHandle;
+
+public:
+	UGameHUDScreen();
+
 	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
 	UCompassBarUserWidget* GetCompassBarWidget() const;
 
+	UFUNCTION(BlueprintCallable, Category = "GameHUDScreen")
+	void FadeScreen(EGameHUDScreenFadeDirection Direction, float Rate = 1.0f);
+
+	UFUNCTION(BlueprintCallable, Category = "GameHUDScreen")
+	void FadeScreenAfterDelay(EGameHUDScreenFadeDirection Direction, float Rate = 1.0f, float DelayDuration = 0.2f);
+
 protected:
-	void NativeOnInitialized() override;
+	void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+
+private:
+	void BeginScreenFade(float TargetRenderOpacity, float FadeRate);
+
+	void OnScreenFadeOutComplete();
+	void OnScreenFadeInComplete();
 };
