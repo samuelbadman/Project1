1a1fa5e1a78cdcae088ab0429054c54c3ceca284
samuelbadman <samuelbadman@outlook.com>
2025-04-14 16:10:25 +0100

    Setup prototype male character and character animation instance classes. Imported character skeletal mesh and idle animation assets.

---
+++ Source/Project1/Public/AnimInstances/PrototypeMaleCharacterAnimInst.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Project1AnimInstanceBase.h"
+#include "PrototypeMaleCharacterAnimInst.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UPrototypeMaleCharacterAnimInst : public UProject1AnimInstanceBase
+{
+	GENERATED_BODY()
+	
+};
