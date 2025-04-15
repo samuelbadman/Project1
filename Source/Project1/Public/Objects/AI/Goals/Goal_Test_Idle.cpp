36a3a277e9be982d2b2fabc819351d63322a891f
samuelbadman <samuelbadman@outlook.com>
2025-04-15 16:37:27 +0100

    Added example of sampling AI navigation path points from the nav mesh.

--- Source/Project1/Public/Objects/AI/Goals/Goal_Test_Idle.cpp
+++ Source/Project1/Public/Objects/AI/Goals/Goal_Test_Idle.cpp
@@ -7,9 +7,11 @@
 #include "NavigationSystem.h"
 #include "AI/Navigation/NavigationTypes.h"
 #include "Actors/Pawns/Characters/Project1CharacterBase.h"
+#include "NavigationPath.h"
 
 UGoal_Test_Idle::UGoal_Test_Idle()
-	: bJustMoved(false)
+	: 
+	bJustMoved(false)
 {
 }
 
@@ -24,6 +26,7 @@ void UGoal_Test_Idle::ReevaluateSubGoal(TObjectPtr<AProject1AIControllerBase> AI
 	{
 		const TObjectPtr<UGoal_Common_Wait> WaitGoal{ AI->CreateGoal<UGoal_Common_Wait>(UGoal_Common_Wait::StaticClass()) };
 		WaitGoal->Initialize(2.0f);
+
 		AI->AddSubGoal(WaitGoal);
 		bJustMoved = false;
 	}
@@ -36,9 +39,21 @@ void UGoal_Test_Idle::ReevaluateSubGoal(TObjectPtr<AProject1AIControllerBase> AI
 void UGoal_Test_Idle::AddMoveToRandomReachablePointSubGoal(TObjectPtr<AProject1AIControllerBase> AI)
 {
 	const TObjectPtr<UGoal_Common_MoveToLocation> MoveToLocationGoal{ AI->CreateGoal<UGoal_Common_MoveToLocation>(UGoal_Common_MoveToLocation::StaticClass()) };
+
 	FNavLocation TargetMoveLocation;
 	FNavigationSystem::GetCurrent<UNavigationSystemV1>(GetWorld())->GetRandomReachablePointInRadius(FVector::ZeroVector, 2048.0f, TargetMoveLocation);
+
+	// Get path points from the AI pawn's current location and target move location. Will be helpful if implementing root motion navigation system.
+	// Requires NavigationPath.h header for UNavigationPath object returned from FindPathToLocationSynchronously
+	//const TObjectPtr<UNavigationPath> NavPath = UNavigationSystemV1::FindPathToLocationSynchronously(this, AI->GetPawn()->GetActorLocation(), TargetMoveLocation);
+	//DrawDebugSphere(GetWorld(), NavPath->PathPoints[0], 5.0f, 24, FColor::Red, false, 10.0f);
+	//for(int32 i = 1; i < NavPath->PathPoints.Num(); ++i)
+	//{
+	//	DrawDebugSphere(GetWorld(), NavPath->PathPoints[i], 5.0f, 24, FColor::Green, false, 10.0f);
+	//}
+
 	MoveToLocationGoal->Initialize(TargetMoveLocation.Location, CastChecked<AProject1CharacterBase>(AI->GetCharacter())->GetLinearWalkSpeed(), 50.0f);
+
 	AI->AddSubGoal(MoveToLocationGoal);
 	bJustMoved = true;
 }
