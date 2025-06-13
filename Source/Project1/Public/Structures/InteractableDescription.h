69fab0c6a1e35178ef36a9d75b29ee0cbab9ad13
samuelbadman <samuelbadman@outlook.com>
2025-06-13 15:46:03 +0100

    Updated comments.

--- Source/Project1/Public/Structures/InteractableDescription.h
+++ Source/Project1/Public/Structures/InteractableDescription.h
@@ -27,9 +27,10 @@ struct FInteractableDescription
 	float TapInteractionPercentCompleteIncrementAmount{ 0.2f };
 
 	// Curve float asset used to modify the TapInteractionPercentCompleteIncrementAmount based on the completion percentage of the interaction. The curve should be in the time range
-	// 0 - 1 and sampled values are multiplied with TapInteractionPercentCompleteIncrementAmount. Time values outside the 0 - 1 range are not sampled. For example, the curve would 
-	// usually contain a point starting at 0, 1 and an ending point at 1, X. X is the final value multiplied with TapInteractionPercentCompleteIncrementAmount as the interaction
-	// completion percent increases. Points in between these two describe how the multiplier gets to the final value X. Only used when interaction type is set to tap
+	// 0 - 1 and sampled values are multiplied with TapInteractionPercentCompleteIncrementAmount. Time values outside the 0 - 1 range are not sampled. The curve only gets sampled
+	// when the interact input is started (pressed) For example, the curve would usually contain a point starting at 0, 1 and an ending point at 1, X. X is the final value 
+	// multiplied with TapInteractionPercentCompleteIncrementAmount as the interaction completion percent increases. Points in between these two describe how the multiplier gets 
+	// to the final value X. Only used when interaction type is set to tap
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "InteractableDescription")
 	TObjectPtr<UCurveFloat> TapInteractionIncrementAmountCurveModifier{ nullptr };
 
