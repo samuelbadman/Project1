2e987cafd0a95ab7551062ee814153ed5b761222
samuelbadman <samuelbadman@outlook.com>
2025-06-25 14:25:13 +0100

    Updated project to engine version 5.6.

--- Source/Project1Editor.Target.cs
+++ Source/Project1Editor.Target.cs
@@ -9,7 +9,7 @@ public class Project1EditorTarget : TargetRules
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
 		ExtraModuleNames.Add("Project1");
 	}
 }
